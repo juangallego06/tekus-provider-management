6edaf3bc110b525ba4c19e865158b2bebbc222ae
jag062002 <juanchogallego2010@hotmail.com>
2026-06-16 10:41:55 -0500

    feat: add global exception handling middleware

---
+++ Api/Tekus.Api/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Tekus.Api.Middlewares;
+
+namespace Tekus.Api.Extensions;
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder
+        UseGlobalExceptionHandling(
+            this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<
+            ExceptionHandlingMiddleware>();
+    }
+}
---
+++ Api/Tekus.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Tekus.Api.Middlewares;
+
+public sealed class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(
+        RequestDelegate next,
+        ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(
+                exception,
+                exception.Message);
+
+            context.Response.StatusCode =
+                (int)HttpStatusCode.InternalServerError;
+
+            context.Response.ContentType =
+                "application/json";
+
+            var response = new
+            {
+                message = exception.Message
+            };
+
+            await context.Response.WriteAsync(
+                JsonSerializer.Serialize(response));
+        }
+    }
+}
