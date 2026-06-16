9096a910f671b818546fe06c4502c7606cbc3a41
jag062002 <juanchogallego2010@hotmail.com>
2026-06-16 10:28:37 -0500

    feat: add request validation layer

---
+++ Api/Tekus.Application/Common/Response.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation.Results;
+
+namespace Tekus.Application.Common;
+
+public class Response<T>
+{
+    public T? Data { get; set; }
+
+    public bool IsSuccess { get; set; }
+
+    public string Message { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationFailure>? Errors { get; set; }
+}
---
+++ Api/Tekus.Application/Validators/CreateServiceDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Tekus.Application.DTOs.Service;
+
+namespace Tekus.Application.Validators;
+
+public class CreateServiceDtoValidator
+    : AbstractValidator<CreateServiceDto>
+{
+    public CreateServiceDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(200);
+
+        RuleFor(x => x.HourlyRate)
+            .GreaterThan(0);
+
+        RuleFor(x => x.ProviderId)
+            .GreaterThan(0);
+    }
+}
